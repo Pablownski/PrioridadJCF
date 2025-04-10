ab2fe981b7d5e9fcba0be0ee501ae1a606c14270
Juan Salguero <sal24374@uvg.edu.gt>
2025-04-09 22:19:14 -0600

    .

--- src/test/java/hospital/Tests.java
+++ src/test/java/hospital/Tests.java
@@ -7,25 +7,21 @@ import java.util.PriorityQueue;
 public class Tests {
 
     @Test
-    public void testAddPaciente() {
+    public void testAgregarPaciente() {
         PriorityQueue<Paciente> cola = new PriorityQueue<>();
         Paciente p = new Paciente("Juan", "fractura", 'C');
         cola.add(p);
 
-        // La cola no debe estar vacía
         assertFalse(cola.isEmpty());
-
-        // El primer elemento debe ser el que agregamos
         assertEquals("Juan", cola.peek().getNombre());
     }
 
     @Test
-    public void testRemovePaciente() {
+    public void testEliminarPaciente() {
         PriorityQueue<Paciente> cola = new PriorityQueue<>();
         Paciente p = new Paciente("Juan", "fractura", 'C');
         cola.add(p);
 
-        // Eliminamos al paciente
         Paciente eliminado = cola.poll();
 
         assertEquals("Juan", eliminado.getNombre());
